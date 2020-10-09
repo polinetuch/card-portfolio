53232544c71d4d902cdc60191aa171dee5d3e775
Polinet <uchpolinet@gmail.com>
2020-10-09 15:26:26 +1100

    installed react storybook

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import LandingPage from './components/LandingPage/LandingPage';
 const App = () => {
   return (
     <div className="App">
+      <div className='navbar'>
+        <h1>deboodle</h1>
+      </div>
       <LandingPage/>
     </div>
   );
