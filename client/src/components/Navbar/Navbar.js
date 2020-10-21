e59dfb9dbfdb961e1784fe612d442be0dcdfbfd2
Polinet <uchpolinet@gmail.com>
2020-10-21 23:13:39 +1100

    added padding-top in Cards+Illustration & height in SideDrawer

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = (props) => {
                 <div>
                     <a href='/' className='navbar__logo'>
                         <img 
-                            className='navbar__logo-image'
+                            className='navbar__logo-image   '
                             src='https://github.com/polinetuch/deboodle/blob/main/client/src/components/asset-images/deboodle_logo.jpg?raw=true' 
                             alt='Deboole Logo'/>
                     </a>
