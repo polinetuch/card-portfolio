d45893d74531ae65ec38ac45527f82d70af73821
Polinet <uchpolinet@gmail.com>
2020-10-26 21:53:33 +1100

    changed styling color and added img

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -4,8 +4,10 @@ import './LandingPage.scss';
 const LandingPage = () => {
     return (
         <div className='landing-page'>
-            <h1>Welcome to deboodle.</h1>
-            <button className='landing-page__button'>Browse Cards</button>
+            <div className='landing-page__div'>
+                <h1>Welcome to deboodle.</h1>
+                <button className='landing-page__button'>Browse Cards</button>
+            </div>
         </div>
     )
 };
