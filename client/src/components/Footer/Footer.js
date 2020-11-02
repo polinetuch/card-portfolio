48c23bece8918cf62c2d60edfdad83c6e5f358c3
Polinet <uchpolinet@gmail.com>
2020-11-02 17:54:50 +1100

    changed styling of footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,14 @@ import './Footer.scss';
 const Contact = () => {
     return (
         <div className='contact__container' id='contact'>
-            
+            <h1>Contact Us To Find Out More</h1>
+            <div className='contact__lists'>
+                <img src='/footer-logo.jpg'/>   
+                <ul>
+                    <li type='email'>Email: DEBOODLE1020@GMAIL.COM</li>
+                    <li>Instagram: @DEBOODLE_</li>
+                </ul>
+            </div>
         </div>
     )
 };
