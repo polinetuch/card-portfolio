8182eeb0624ef4baa4e5986ea11d6c8ecc47881f
Polinet <uchpolinet@gmail.com>
2020-11-18 14:13:09 +1100

    show image when clicked

---
+++ client/src/components/ImageCategories/AllImageCategories.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './AllImageCategories.scss';
+
+const AllImageCategories = ({collapsed, children}) => {
+    const [isCollapsed, setIsCollapsed] = useState(collapsed);
+
+    return (
+        <div id='gallery'>
+            <div
+                className="collapse-button"
+                onClick={() => setIsCollapsed(!isCollapsed)}
+            >
+                {isCollapsed ? 'Show' : 'Hide'} {children.name}
+            </div>
+            <div
+                className={`collapse-content ${isCollapsed ? 'collapsed' : 'expanded'}`}
+                aria-expanded={isCollapsed}
+            >
+                {children}
+            </div>
+
+        </div>
+    )
+}
+
+export default AllImageCategories;
