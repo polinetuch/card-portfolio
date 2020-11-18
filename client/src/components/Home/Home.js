7795b7230a219f193741de0ea263b57e24dc3301
Polinet <uchpolinet@gmail.com>
2020-11-18 13:04:20 +1100

    added components to display diff card categories

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LandingPage from '../LandingPage/LandingPage';
 import Service from '../Service/Service';
-import ImageCategories from '../ImageCategories/AllCategories/AllCategories';
+import Illustrations from '../ImageCategories/Illustrations';
+import Christmas from '../ImageCategories/Christmas';
+import HandDrawnCalligraphy from '../ImageCategories/HandDrawnCalligraphy';
 import Contact from '../Footer/Footer';
 
 const Home = () => {
+    const [isCollapsed, setIsCollapsed] = useState(collapsed);
+
     return (
         <div>
             <LandingPage/>
             <Service/>
-            <ImageCategories/>
+            <button onClick={() => setIsCollapsed(!collapsed)}>
+                {isCollapsed ? 'Show' : 'Hide'}
+
+            </button>
+            <Christmas/>
+            <Illustrations/>
+            <HandDrawnCalligraphy/>
             <Contact/>
         </div>
     )
---
+++ client/src/components/ImageCategories/Illustrations.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import images from './json/illustration.json';
+import './ImageCategories.scss';
+
+const illustrations = () => {
+    const showAllImages = images.map((element, index) => {
+        return (
+            <figure key={index}>
+                <img className='illustration__images' src={element.image} alt={element.name}/>
+            </figure>
+        )
+    })
+    return (
+        <div className='illustration__container'>
+            <h1>Illustrations</h1>
+            <div className='illustration__image-container'>
+                {showAllImages}
+            </div>
+        </div>
+    )
+};
+
+export default illustrations;
